67c1ff8007643f3c8fe02b93ca142f9ce24c0cbc
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-07-15 00:11:36 -0300

    Criando opçoes e capturando o valor digitado

--- main.dart
+++ main.dart
@@ -1,6 +1,13 @@
+import 'dart:io';
 void main(){
+  menu();
 }
 
 void menu(){
-
+ print('Digite [1] para somar');
+ print('Digite [2] para subtrair');
+ print('Digite [3] para multiplicar');
+ print('Digite [4] para dividir');
+ print('Digite [5] para sair');
+ String? entradaOp = stdin.readLineSync();
 }
