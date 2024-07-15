79c367d17e7510678cddd36151cd15ea0b398740
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-07-15 00:23:54 -0300

    Tratando o valor de  opcao

--- main.dart
+++ main.dart
@@ -10,4 +10,25 @@ void menu(){
  print('Digite [4] para dividir');
  print('Digite [5] para sair');
  String? entradaOp = stdin.readLineSync();
+
+ if(entradaOp != null && entradaOp.isNotEmpty){
+   try{
+
+    int opcao = int.parse(entradaOp);
+
+      if ( opcao > 0 &&  opcao < 6) {       // tratando o intervalo de valores válidos
+        
+      } else {
+        print('O valor está fora da faixa. \nO programa será encerrado');
+        exit(0);
+      }
+
+   }catch(e){
+    throw Exception('ERRO! o valor $entradaOp não pode ser convertido');
+   }
+ }else{
+   print('ERRO! Valor inválido o programa será encerrado');
+   exit(0);
+ }
+
 }
