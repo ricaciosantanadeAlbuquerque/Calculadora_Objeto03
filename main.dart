34ec28a6f291f6aaa7f43098ed83b2152716764d
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-07-15 00:36:02 -0300

    Tratando os valores de entrada da operação

--- main.dart
+++ main.dart
@@ -17,7 +17,33 @@ void menu(){
     int opcao = int.parse(entradaOp);
 
       if ( opcao > 0 &&  opcao < 6) {       // tratando o intervalo de valores válidos
-        
+
+        print('Digite o primeiro valor');
+        String? entradaValor1 = stdin.readLineSync();
+        print('Digite o segundo valor');
+        String? entradavalor2 = stdin.readLineSync();
+
+        if(entradaValor1 != null && entradavalor2 != null ){
+          if(entradaValor1.isNotEmpty && entradavalor2.isNotEmpty){
+             try{
+                
+               double valor1 = double.tryParse(entradaValor1) ?? 0.0;
+               double valor2 = double.tryParse(entradavalor2) ?? 0.0;
+               
+             }catch(e){
+
+              throw Exception('ERRO! $entradaValor1 ou $entradavalor2 não podem ser convertidos');
+
+             }
+          }else{
+            print('ERRO! Valor inválido o programa será encerrado');
+            exit(0);
+          }
+        }else{
+          print('ERRO! Valor inválido o programa será encerrado');
+          exit(0);
+        }
+
       } else {
         print('O valor está fora da faixa. \nO programa será encerrado');
         exit(0);
