d2257d57e0fbad7dddd177cb7bf733669c60100d
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-07-15 01:19:23 -0300

    Interface Calculadora finalizada

--- interface_calculadora.dart
+++ interface_calculadora.dart
@@ -4,6 +4,9 @@ abstract class InterfaceCalculadora{
   double? multiplicacao;
   double? divisao;
 
-  
+  double somaValor({required double valor1, required double valor2});
+  double subtracaoValor({required double valor1, required double valor2});
+  double multiplicacaoValor({required double valor1, required double valor2});
+  double divisaoValor({required double valor1, required double valo2});
 
 }
